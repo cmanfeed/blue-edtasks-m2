222594c55ae0a13c9e427586b0b856ff2796d57b
caiomanfredini <caiomanfredini.15@gmail.com>
2021-07-21 18:44:00 -0300

    att portfólio

---
+++ Aulas/Aula 6/app.js
@@ -0,0 +1,11 @@
+function viradaPaulista() {
+    alert('Você pediu Virada à Paulista!');
+}
+
+function estrogonoffe() {
+    alert('Você pediu Estrogonoffe!');
+}
+
+function jabuticaba() {
+    alert('Você pediu Jabuticaba!');
+}
