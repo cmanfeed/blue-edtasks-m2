831c32fd94c6ab4721960465408b739741dda26d
caiomanfredini <caiomanfredini.15@gmail.com>
2021-07-21 23:11:21 -0300

    att aula 8

--- Aulas/Aula 8/static/js/script.js
+++ Aulas/Aula 8/static/js/script.js
@@ -1,13 +1,11 @@
 const tasks = document.querySelectorAll('.task');
 
 tasks.forEach((task) => {
-    task.addEventListener('click', () =>{
-        if (task.style.textDecoration === 'line-through'){
+    task.addEventListener('click', () => {
+        if (task.style.textDecoration === 'line-through') {
             task.style.textDecoration = 'none';
-        }else{
-            task.style.textDecoration = 'line-through'
+        } else {
+            task.style.textDecoration = 'line-through';
         }
-
-        ;
     })
 })
